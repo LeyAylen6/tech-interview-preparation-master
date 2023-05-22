80c2451ecc4f392ad57011c29110a53969da133a
Leila <leiisalguero@gmaill.com>
2023-05-22 10:57:52 -0300

    add exercises 11 and 12

--- Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
+++ Ejercicios/11-ClockMinuteAdder/ClockMinuteAdder.js
@@ -1,6 +1,58 @@
 function clockMinuteAdder (time, minutesToAdd) {
-  // Your code here:
+
+  let hours = time[0] + time[1];
+  let minutes = time[3] + time[4];
+
+  let totalMinutes = +minutes + minutesToAdd;
+
+  if (totalMinutes >= 60) {
+    minutes = totalMinutes - 60
+    hours = +hours + 1
+
+  } else minutes = totalMinutes
+
+  if(minutes < 10) minutes = '0' + minutes
+  if(hours < 10) hours = '0' + hours
+  
+              // ? NO FUNCIONA.
+
+  return `${hours}:${minutes}`
 
 }
+
+function clockMinuteAdder (time, minutesToAdd) {
+  let hours = time[0] + time[1];
+  let minutes = time[3] + time[4];
+
+  let totalMinutes = +minutes + minutesToAdd;
+  let totalHours = +hours + Math.floor(totalMinutes/60);
+
+  totalMinutes = totalMinutes % 60
+  totalHours = (totalHours - 1) % 12 + 1;
+
+  if(totalMinutes < 10) totalMinutes = '0' + totalMinutes;
+  if(totalHours < 10) totalHours = '0' + totalHours;
+
+  return `${totalHours}:${totalMinutes}`
+}
+
+// Complejidad >>> O(n**0) = O(1)
+
+function clockMinuteAdder (time, minutesToAdd) {
+  let [hours, minutes] = time.split(':')
+
+  let totalMinutes = +minutes + minutesToAdd;
+  let totalHours = +hours + Math.floor(totalMinutes/60);
+
+  totalMinutes %= 60
+  totalHours = (totalHours - 1) % 12 + 1;
+
+  if(totalMinutes < 10) totalMinutes = '0' + totalMinutes;
+  if(totalHours < 10) totalHours = '0' + totalHours;
+
+  return `${totalHours}:${totalMinutes}`
+}
+
+console.log(clockMinuteAdder('07:00', 155))
 
 module.exports = clockMinuteAdder
--- Ejercicios/12-Intersection/Intersection.js
+++ Ejercicios/12-Intersection/Intersection.js
@@ -1,6 +1,78 @@
+// ! Solución fuerza bruta
 function intersection (arr1, arr2) {
-  // Your code here:
+  // Dado dos arreglos **ordenados** devuelve un arreglo con los números que se repiten.
 
+  const result = [];
+
+  for(const num1 of arr1) {
+    for(const num2 of arr2) {
+      if (num1 == num2) result.push(num1)
+    }
+  }
+  return result;
 }
 
+// Complejidad >>> O(n*m)
+
+// ! Refactor
+function intersection (arr1, arr2) {
+  // Dado dos arreglos **ordenados** devuelve un arreglo con los números que se repiten.
+
+  const result = [];
+
+  for(const num1 of arr1) {
+      if (arr1.includes(num1)) result.push(num1)
+    }
+
+  return result;
+}
+
+// Complejidad >>> O(n*m)
+
+// ! Re-Refactor
+function intersection (arr1, arr2) {
+  // Dado dos arreglos **ordenados** devuelve un arreglo con los números que se repiten.
+  let result = []
+
+  arr1.map(num1 => arr2.includes(num1) && result.push(num1))
+
+  return result;
+}
+
+// Complejidad >>> O(n*m)
+
+// ! Re-Refactor
+function intersection (arr1, arr2) {
+  // Dado dos arreglos **ordenados** devuelve un arreglo con los números que se repiten.
+
+  return arr1.filter(num1 => arr2.includes(num1))
+}
+
+// Complejidad >>> O(n*m)
+
+// ! Un solo bucle para ambos arrays
+function intersection (arr1, arr2) {
+  let i = 0;
+  let j = 0;
+  const result = [];
+
+  while(i < arr1.length && j < arr2.length) {
+    const num1 = arr1[i];
+    const num2 = arr2[i];
+
+    if(num1 === num2) {
+      result.push(num1);
+      i++;
+      j++;
+    }
+
+    else if (num1 < num2) i++;
+    else j++;
+  }
+  return result;
+}
+
+// Complejidad >>> O(n)
+
+console.log(intersection([1,3,5,7,10], [2,3,6,8,10,20]))
 module.exports = intersection
