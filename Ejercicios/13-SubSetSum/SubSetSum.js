277f48d8c78fd899a41bccd7fea7912cb5447e07
Leila <leiisalguero@gmaill.com>
2023-05-26 10:53:40 -0300

    add exercise 13 and 15

--- Ejercicios/13-SubSetSum/SubSetSum.js
+++ Ejercicios/13-SubSetSum/SubSetSum.js
@@ -1,6 +1,36 @@
-function subsetSum (nums, n) {
-  // Your code here:
+function subSetSum (nums, target) {
+  
+  let sumas = [0]
 
+  for (const num of nums) {
+    for (const sum of sumas) { // Si usamos sumas siempre le estamos agregando un lugar mas entonces nunca se termina de recorrer el arr.
+      const result = num + sum
+
+      if(result === target) return true
+      sumas.push(result)
+    }
+  }
+  return false
 }
 
-module.exports = subsetSum
+function subSetSum (nums, target) {
+  
+  let sumas = [0]
+
+  for (const num of nums) {
+    for (const sum of sumas) { // Si usamos sumas siempre le estamos agregando un lugar mas entonces nunca se termina de recorrer el arr.
+      const result = num + sum
+
+      if(result === target) return true
+      result < target && sumas.push(result)
+    }
+  }
+  return false
+}
+
+console.log(subSetSum([1, 10, 5, 3], 9)) // (true))
+nsole.log(subSetSum([1, 10, 5, 3], 10)) // (true))
+console.log(subSetSum([1, 10, 5, 3], 19)) // (true))
+console.log(subSetSum([1, 10, 5, 3], 2)) // (false))
+
+module.exports = subSetSum
--- Ejercicios/15-SpyOn/SpyOn.js
+++ Ejercicios/15-SpyOn/SpyOn.js
@@ -1,6 +1,58 @@
 function spyOn (fn) {
-  // Your code here:
+  
+  let callCount = 0;
+  const calledWidth = new Map();
+  const returned = new Map();
 
+  const spy = (n1, n2) => {
+    callCount++;              // Aumenta el contador cada ves que se ejecuta.
+    calledWidth.set(n1, true)
+    calledWidth.set(n2, true) // Guarda los parametros que recibio
+
+    returned.set(fn(n1, n2), true) // Guarda lo que retorna
+
+    return fn(n1, n2)
+  }
+
+  spy.getCallCount = () => callCount;
+
+  spy.wasCalledWith = (value) => {
+    if(calledWidth.get(value)) return true;
+    else return false;
+  }
+
+  spy.returned = (value) => {
+    if(returned.get(value)) return true;
+    else return false;
+  }
+
+  return spy;
+}
+
+// ! Refactor
+
+function spyOn (fn) {
+  
+  let callCount = 0;
+  const calledWidth = new Map();
+  const returned = new Map();
+
+  const spy = (...args) => {
+    callCount++; // Aumenta el contador cada ves que se ejecuta.
+
+    for(const arg of args) calledWidth.set(arg, true);  // Guarda los parametros que recibio
+
+    const result = fn(...args); // Guarda lo que retorna
+    returned.set(result, true); //       ""
+
+    return result;
+  }
+  
+  spy.getCallCount = () => callCount;
+  spy.wasCalledWith = (value) => !!calledWidth.get(value)
+  spy.returned = (value) => !!returned.get(value)
+
+  return spy;
 }
 
 module.exports = spyOn
