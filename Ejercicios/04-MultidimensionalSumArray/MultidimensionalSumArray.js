56812a349ae4062ee93882bb8314bba3df532d47
Leila <leiisalguero@gmaill.com>
2023-06-02 16:10:25 -0300

    add exercise 04 and unskipped all test

--- Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
+++ Ejercicios/04-MultidimensionalSumArray/MultidimensionalSumArray.js
@@ -1,6 +1,30 @@
-function mdArraySum (arr) {
-  // Your code here:
+// // ! Solución de fuerza bruta
+// function mdArraySum (arr) {
+  
+//   let count = 0;
 
+//   for(element of arr) {
+//     if (typeof element === 'number') count += element;
+//     else count += mdArraySum(element)
+//   }
+//   return count;
+// }
+
+// // ! Refactor
+// function mdArraySum (arr, count = 0) {
+  
+//   for(element of arr) {
+//     count = typeof element === 'number' ? count + element : mdArraySum(element, count)
+//   }
+//   return count;
+// }
+
+// ! Refactor 2
+function mdArraySum (arr, count) {
+
+  return arr.reduce((count, element) => count + (typeof element === 'number' ? count + element : mdArraySum(element, count)), 0);
 }
+
+// Complejidad >> O(n^m) --> es una complejidad mayor al log(n)
 
 module.exports = mdArraySum
