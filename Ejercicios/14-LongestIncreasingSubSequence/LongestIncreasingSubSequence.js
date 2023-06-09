70fac425cc5325c243797044c1d6f42236f06f03
Leila <leiisalguero@gmaill.com>
2023-06-09 11:40:01 -0300

    add solved exercise 14

--- Ejercicios/14-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
+++ Ejercicios/14-LongestIncreasingSubSequence/LongestIncreasingSubSequence.js
@@ -1,6 +1,66 @@
+// ! Solucion fuerza bruta MIA, Falta que sean muchas secuencias
+// function longestIncreasingSubsequence (nums) {
+//   const secuences = [];
+
+//   for (let i = 1 ; i < nums.length - 1; i++) {
+//     let j = i - 1
+//     if (!secuences[secuences.length-1]) {
+//       if (nums[j] < nums[i]) secuences.push(nums[j])
+    
+//     } else {
+//       if (nums[j] > secuences[secuences.length - 1]) secuences.push(nums[j])
+//     }
+    
+//   }
+//   console.log(secuences)
+// // Secuencia: 10, 22, 33, 41, 60, 80;  output: 9
+// }
+
+// ! Solucion fuerza bruta
+// function longestIncreasingSubsequence (nums) {
+
+//   const secuences = [];
+//   let longest = 0;
+
+//   for(num of nums) {
+//     secuences.push([num]);
+//     const secuencesCopy = [...secuences];
+
+//     for( const seq of secuencesCopy) {
+//       if (num > seq[seq.length - 1]) {
+//         const newSequence = seq.concat(num);
+//         secuences.push(newSequence)
+        
+//         if(newSequence.length > longest) longest = newSequence.length
+//       }
+//     }
+//   }
+//   return longest;
+// }
+// Complejidad >>> O(n*m + x) , Con mas porque el ultimo for no esta anidado, sino que esta aparte.
+
+// ! Solucion Refactor
 function longestIncreasingSubsequence (nums) {
-  // Your code here:
 
+  const secuences = [];
+  let longest = 0;
+
+  for(num of nums) {
+    const secuencesCopy = [...secuences]; // si lo hacemos antes del push evitamos repetir numeros
+    secuences.push([num]);
+
+    for( const seq of secuencesCopy) {
+      if (num > seq[seq.length - 1]) {
+        const newSequence = [...seq, num]
+        secuences.push(newSequence)
+        
+        if(newSequence.length > longest) longest = newSequence.length
+      }
+    }
+  }
+  return longest;
 }
+// Complejidad >>> O(n*m)
 
+// console.log(longestIncreasingSubsequence([3, 4, 2, 1, 10, 6]))
 module.exports = longestIncreasingSubsequence
